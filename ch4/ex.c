585575e54ee419c1aefd386dfcd46bc34354dcf9
Aleksey Khaustov <a.khaustov13@gmail.com>
2017-04-02 13:38:50 +0300

    ch4 added

---
+++ ch4/ex.c
@@ -0,0 +1,10 @@
+
+
+int arr[100];
+
+int main()
+{
+  int arr1[32];
+  return 0;
+}
+
---
+++ ch4/ex.s
@@ -0,0 +1,41 @@
+	.file	"ex.c"
+	.comm	arr,400,32
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
+	subl	$148, %esp
+	movl	%gs:20, %eax
+	movl	%eax, -12(%ebp)
+	xorl	%eax, %eax
+	movl	$0, %eax
+	movl	-12(%ebp), %edx
+	xorl	%gs:20, %edx
+	je	.L3
+	call	__stack_chk_fail
+.L3:
+	addl	$148, %esp
+	popl	%ecx
+	.cfi_restore 1
+	.cfi_def_cfa 1, 0
+	popl	%ebp
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
